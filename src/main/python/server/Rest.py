e5bf24edb1eda63514226dce152f8e5024a21e43
Alexander Hasenberger <ahasenberger@student.tgm.ac.at>
2018-11-26 09:04:36 +0100

    Parameter query from rest, db connection works

--- src/main/python/server/Rest.py
+++ src/main/python/server/Rest.py
@@ -1,25 +1,42 @@
-from flask import Flask
+import sqlite3 as lite
+import sys
+from flask import Flask, request
 app = Flask(__name__)
+
+con = lite.connect('Students')
 
 @app.route('/')
 def hello_world():
    return "Hello World"
 
-@app.route("/user/get/<string:name>/")
-def getMember(name):
-   return "User hinzufügen %s" (name)
+@app.route("/user/get/")
+def getMember():
+   with con:
+      cur = con.cursor()
+      users = cur.execute("SELECT * from students")
+   return users
 
-@app.route("/user/add/<string:name>/")
-def addMember(name):
-   return "Members"
+@app.route('/useradd/')
+def addMember():
+   #with con:
+   with lite.connect('Students') as con:
+      username = request.args['username']
+      email = request.args['email']
+      cur = con.cursor()
+      cur.execute("INSERT INTO students (username, email) VALUES (?)", (username, email))
+      con.commit()
+   #cur = con.cursor()
+   #cur.execute("INSERT INTO students (username, email) VALUES(usernameinput, emailinput)")
 
-@app.route("/user/update/<string:name>/")
-def updateMember(name):
-   return name
+   return "user"
 
-@app.route("/user/delete/<string:name>/")
-def delMember(name):
-   return name
+@app.route("/user/update/<string:nameupdate>/")
+def updateMember(nameupdate):
+   return nameupdate
+
+@app.route("/user/delete/<string:namedel>/")
+def delMember(namedel):
+   return namedel
 
 if __name__ == '__main__':
    app.run()
